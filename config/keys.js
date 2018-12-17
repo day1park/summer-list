d8c6ff842f5ec9c75aecbe2f1da973ddbe8591ed
day1park <daypark2408@gmail.com>
2018-12-18 08:55:30 +1100

    add mlab user

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://nomad:summer1@ds237574.mlab.com:37574/mern_summer"
+};
--- server.js
+++ server.js
@@ -7,3 +7,17 @@ const app = express();
 // Bodyparser Middleware
 
 app.use(bodyParser.json());
+
+// DB config
+
+const db = require("./config/keys").mongoURI;
+
+//connect to mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("mongoDB connected!"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server started on port ${port}`));
